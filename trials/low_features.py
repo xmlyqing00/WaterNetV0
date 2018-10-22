d19a55f933bcaf61431aa2752d3d98d94fe2f45a
yq <yqliang@cct.lsu.edu>
2018-10-22 14:10:49 -0500

    Update trials

--- trials/low_features.py
+++ trials/low_features.py
@@ -1,6 +1,7 @@
 import sys
 import cv2
 import os
+import numpy as np
 from matplotlib import pyplot as plt
 
 sys.path.append('../')
@@ -8,21 +9,39 @@ sys.path.append('../')
 def trial_gradient(img):
     
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    laplacian = abs(cv2.Laplacian(gray_img, cv2.CV_32F))
-    sobelx = abs(cv2.Sobel(gray_img, cv2.CV_32F, 1, 0, ksize=3))
-    sobely = abs(cv2.Sobel(gray_img, cv2.CV_32F, 0, 1, ksize=3))
 
-    cv2.imshow("img", img) 
-    cv2.imshow("gray", gray_img)
-    cv2.imshow("laplacian", laplacian)
-    cv2.imshow("sobelx", sobelx)
-    cv2.imshow("sobely", sobely)
-    cv2.waitKey()
+    f = np.fft.fft2(gray_img)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+    rows, cols = gray_img.shape
+    crow,ccol = (int)(rows/2) , (int)(cols/2)
+    w = 50
+    fshift[crow-w:crow+w, ccol-w:ccol+w] = 0
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = np.fft.ifft2(f_ishift)
+    img_back = np.abs(img_back)
+
+    plt.subplot(131),plt.imshow(img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
+    plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
+    plt.subplot(133),plt.imshow(img_back)
+    plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+    # plt.subplot(121),plt.imshow(gray_img, cmap = 'gray')
+    # plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    # plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    # plt.show()
 
 if __name__ == '__main__':
 
-    root_folder = '/Ship01/Dataset/flood/collection/'
-    img_path = os.path.join(root_folder, 'stream_test/imgs/original_resize_14.png')
+    # root_folder = '/Ship01/Dataset/flood/collection/'
+    # img_path = os.path.join(root_folder, 'stream_test/imgs/original_resize_14.png')
+    img_path = 'original_resize_50.png'
     img = cv2.imread(img_path)
     img = cv2.resize(img, dsize=None, fx=0.5, fy=0.5)
 
